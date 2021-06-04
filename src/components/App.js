a5b4e912ed1b16b2a8bd21f432f1448e9e7c67bc
Bharath Kumar <bharathkumarmurugan2016@gmail.com>
2021-06-04 11:42:16 +0530

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import './App.css';
 
+function App(){
+  return (
+    <div className="container">
+      Create React App without <code>npx create-react-app</code> command.
+    </div>
+  )
+}
+
+export default App;
